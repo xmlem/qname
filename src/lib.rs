c5dd6cc89a2113a5d2bafaee26da8a0322d5e9f6
Ltrlg <ltrlg.wp@gmail.com>
2024-11-20 23:11:50 +0100

    Add tests for valid cases

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,18 @@ mod tests {
     use super::QName;
 
     #[test]
-    fn qname() {
+    fn invalid() {
         assert!(QName::new("9").is_err());
         assert!(QName::new("").is_err());
         assert!(QName::new("\n").is_err());
         assert!(QName::new("9\n").is_err());
         assert!(QName::new("\n9").is_err());
+    }
+
+    #[test]
+    fn valid() {
+        QName::new("Foo").unwrap();
+        QName::new("foo").unwrap();
+        QName::new("foo-bar").unwrap();
     }
 }
